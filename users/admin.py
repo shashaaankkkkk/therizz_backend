fc1f5de3c8e24560b2f6b7a5e915396188406966
Shashank Shekhar <shashankshekhar8534@gmail.com>
2024-10-31 03:09:09 +0530

    backend

---
+++ users/admin.py
@@ -0,0 +1,7 @@
+# users/admin.py
+from django.contrib import admin
+from .models import User
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_admin', 'is_client')
---
+++ users/serializers.py
@@ -0,0 +1,8 @@
+# users/serializers.py
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'is_admin', 'is_client']
