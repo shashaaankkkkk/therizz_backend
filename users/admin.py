f59af4eb333320818f418b41fc9e787de0d0ef97
Shashank Shekhar <shashankshekhar8534@gmail.com>
2024-10-31 14:48:47 +0530

    added

--- users/admin.py
+++
@@ -1,7 +0,0 @@
-# users/admin.py
-from django.contrib import admin
-from .models import User
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'is_admin', 'is_client')
